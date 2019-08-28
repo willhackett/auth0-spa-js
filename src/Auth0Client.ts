0f7928e8282f7be2fa979bf426ae7a74a27f6111
Luis Deschamps Rudge <luis@luisrudge.net>
2019-08-28 18:26:56 -0300

    max_age

--- src/Auth0Client.ts
+++ src/Auth0Client.ts
@@ -73,7 +73,7 @@ export default class Auth0Client {
   private _authorizeUrl(authorizeOptions: AuthorizeOptions) {
     return this._url(`/authorize?${createQueryParams(authorizeOptions)}`);
   }
-  private _verifyIdToken(id_token: string, nonce?: string, max_age?: string) {
+  private _verifyIdToken(id_token: string, nonce?: string) {
     return verifyIdToken({
       iss: `${this.domainUrl}/`,
       aud: this.options.client_id,
@@ -81,7 +81,7 @@ export default class Auth0Client {
       nonce,
       leeway: this.options.leeway,
       client_id: this.options.client_id,
-      max_age
+      max_age: this.options.max_age
     });
   }
 
